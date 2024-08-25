f0b2a72d0a51f0f9497eef285a6811a03543dbdf
Junior-Shyko <franciscoanto@gmail.com>
2024-08-25 10:36:59 -0300

    Ajustando conf para receber do env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (config('app.env') === 'production') {
+        if (env('APP_ENV') === 'production') {
             \URL::forceScheme('https');
         }
         
